473b552bb4b3e171f377311718a5fcf066f8f37a
nievesdarli8-collab <nievesdarli8@gmail.com>
2026-07-24 11:16:35 -0400

    Actualizar game_flow_engine.dart

--- lib/engine/game_flow_engine.dart
+++ lib/engine/game_flow_engine.dart
@@ -1,32 +1,43 @@
 import '../models/activity.dart';
+import '../models/game_turn.dart';
 import 'activity_engine.dart';
 import 'player_engine.dart';
+import 'turn_engine.dart';
 
 class GameFlowEngine {
   GameFlowEngine._();
 
-  static final GameFlowEngine instance =
-      GameFlowEngine._();
+  static final GameFlowEngine instance = GameFlowEngine._();
 
-  final ActivityEngine activityEngine =
-      ActivityEngine.instance;
-
-  final PlayerEngine playerEngine =
-      PlayerEngine.instance;
+  final ActivityEngine activityEngine = ActivityEngine.instance;
+  final PlayerEngine playerEngine = PlayerEngine.instance;
+  final TurnEngine turnEngine = TurnEngine.instance;
 
   int _turn = 0;
 
   int get currentTurn => _turn;
 
-  void nextTurn() {
-    _turn++;
-  }
+  GameTurn? nextTurn() {
+    final player = turnEngine.currentPlayer(playerEngine.players);
+    final activity = activityEngine.nextActivity();
 
-  Activity? nextActivity() {
-    return activityEngine.nextActivity();
+    if (player == null || activity == null) {
+      return null;
+    }
+
+    final turn = GameTurn(
+      number: ++_turn,
+      player: player,
+      activity: activity,
+    );
+
+    turnEngine.next(playerEngine.players);
+
+    return turn;
   }
 
   void reset() {
     _turn = 0;
+    turnEngine.reset();
   }
 }
