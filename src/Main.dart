9ba9e9886ded82505046c043524dee4fb0172139
nievesdarli8-collab <nievesdarli8@gmail.com>
2026-07-23 20:00:37 -0400

    Crear src/Main.dart

---
+++ src/Main.dart
@@ -0,0 +1,13 @@
+/// ===========================================================
+/// DAREVO
+/// Archivo principal de la aplicación
+/// Versión 0.0.1 Pre-Alpha
+/// ===========================================================
+
+void main() {
+  print("=================================");
+  print("        DAREVO ENGINE");
+  print("     Versión 0.0.1 Pre-Alpha");
+  print("=================================");
+  print("Inicializando proyecto...");
+}
