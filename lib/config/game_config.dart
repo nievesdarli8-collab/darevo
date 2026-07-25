a3b13d2416f3afcc65a66f22454899b0fda76284
nievesdarli8-collab <nievesdarli8@gmail.com>
2026-07-24 21:37:47 -0400

    Crear lib/config/game_config.dart
    Sprint 17

---
+++ lib/config/game_config.dart
@@ -0,0 +1,17 @@
+class GameConfig {
+  const GameConfig._();
+
+  static const String gameName = "DAREVO";
+
+  static const String version = "0.0.1 Pre-Alpha";
+
+  static const int minPlayers = 2;
+
+  static const int maxPlayers = 20;
+
+  static const bool enableAI = true;
+
+  static const bool enableOnline = true;
+
+  static const bool enableLocal = true;
+}
