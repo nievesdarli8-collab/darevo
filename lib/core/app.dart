b37fda1b55e684f2895e2089768f71a78b0eb13c
nievesdarli8-collab <nievesdarli8@gmail.com>
2026-07-24 21:55:21 -0400

    Crear lib/core/app.dart

---
+++ lib/core/app.dart
@@ -0,0 +1,11 @@
+/// ===========================================================
+/// DAREVO APP
+/// Punto central de configuración de la aplicación.
+/// ===========================================================
+
+class DarevoApp {
+  const DarevoApp._();
+
+  static const String name = 'DAREVO';
+  static const String version = '0.1.0 Alpha Foundation';
+}
